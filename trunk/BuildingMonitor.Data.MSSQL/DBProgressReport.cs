efad2d856b52e9151ba6058223dbcdc840c6c384
widensoft <widensoft@05488754-bb51-4eb8-a5b2-a2c07d1d2a58>
2010-10-18 17:01:02 +0000

    Se adiciono la rutina DetalleAvance

--- trunk/BuildingMonitor.Data.MSSQL/DBProgressReport.cs
+++ trunk/BuildingMonitor.Data.MSSQL/DBProgressReport.cs
@@ -14,7 +14,7 @@ namespace BuildingMonitor.Data
 	{
 		public static String DBPlatform()
 		{
-			return "MSSQL";;
+			return "MSSQL";
 		}
 
 		public static DataSet AvancePorcentual_Bloque(int IdProyecto, int IdGrupo)
@@ -23,7 +23,7 @@ namespace BuildingMonitor.Data
 
 			sb.Append("SELECT  pr.Nombre Proyecto,b.Id IdBloque, b.Nombre Bloque,b.Encargado Supervisor,sum((a.AvanceActual - a.AvanceInicial)*cd.Precio/100) MontoEjecutado, convert(decimal(15,2),0) AS MontoPresupuestado,convert(decimal(15,2),0) AS PorcentajeAvance,convert(decimal(15,2),0) AS PesoEspecifico  ");
 			sb.Append("FROM uAvance a,uContratoDetalle cd,uProyecto pr,uBloque b,uObra o,uGrupo g,uListaGrupo lg,uItem i,uListaItem li,uSubItem si ");
-			sb.Append("WHERE   a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and a.IdProyecto = pr.Id and a.IdBloque = b.Id and ");
+			sb.Append("WHERE a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and a.IdProyecto = pr.Id and a.IdBloque = b.Id and ");
 			sb.Append("a.IdObra = o.Id and a.IdGrupo = g.Id and g.IdListaGrupo = lg.Id and a.IdItem = i.Id and i.IdListaItem = li.Id and a.IdSubItem = si.Id ");
 			sb.AppendFormat("and pr.Id ={0} ", IdProyecto);
 			if (IdGrupo > 0) sb.AppendFormat("and g.IdListaGrupo ={0} ", IdGrupo);
@@ -40,7 +40,7 @@ namespace BuildingMonitor.Data
 
 			sb.Append("select  pr.Nombre Proyecto,sum((a.AvanceActual - a.AvanceInicial)*cd.Precio/100) MontoEjecutado ");
 			sb.Append("from uAvance a,uContratoDetalle cd,uProyecto pr,uBloque b,uObra o,uGrupo g,uListaGrupo lg,uItem i,uListaItem li,uSubItem si ");
-			sb.Append("where   a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and  ");
+			sb.Append("where a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and  ");
 			sb.Append("a.IdProyecto = pr.Id and a.IdBloque = b.Id and a.IdObra = o.Id and a.IdGrupo = g.Id and g.IdListaGrupo = lg.Id and ");
 			sb.Append("a.IdItem = i.Id and i.IdListaItem = li.Id and a.IdSubItem = si.Id ");
 			sb.AppendFormat("and pr.Id ={0} ", IdProyecto);
@@ -89,7 +89,7 @@ namespace BuildingMonitor.Data
 			
 			sb.Append("SELECT  pr.Nombre Proyecto,b.Nombre Bloque,b.Encargado Supervisor, o.Id IdObra, o.Nombre Obra,sum((a.AvanceActual - a.AvanceInicial)*cd.Precio/100) MontoEjecutado, convert(decimal(15,2),0) AS MontoPresupuestado,convert(decimal(15,2),0) AS PorcentajeAvance,convert(decimal(15,2),0) AS PesoEspecifico  ");
 			sb.Append("FROM uAvance a,uContratoDetalle cd,uProyecto pr,uBloque b,uObra o,uGrupo g,uListaGrupo lg,uItem i,uListaItem li,uSubItem si ");
-			sb.Append("WHERE   a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and a.IdProyecto = pr.Id and a.IdBloque = b.Id and ");
+			sb.Append("WHERE  a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and a.IdProyecto = pr.Id and a.IdBloque = b.Id and ");
 			sb.Append("a.IdObra = o.Id and a.IdGrupo = g.Id and g.IdListaGrupo = lg.Id and a.IdItem = i.Id and i.IdListaItem = li.Id and a.IdSubItem = si.Id ");
 			sb.AppendFormat("and pr.Id ={0} ", IdProyecto);
 			if ( IdBloque > 0 ) sb.AppendFormat("and b.Id ={0} ", IdBloque);
@@ -110,7 +110,7 @@ namespace BuildingMonitor.Data
 
 			sb.Append("select  pr.Nombre Proyecto,sum((a.AvanceActual - a.AvanceInicial)*cd.Precio/100) MontoEjecutado ");
 			sb.Append("from uAvance a,uContratoDetalle cd,uProyecto pr,uBloque b,uObra o,uGrupo g,uListaGrupo lg,uItem i,uListaItem li,uSubItem si ");
-			sb.Append("where   a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and  ");
+			sb.Append("where a.IdSubItem = cd.IdSubItem and a.IdContrato = cd.IdContrato and o.Plantilla = 0 and  ");
 			sb.Append("a.IdProyecto = pr.Id and a.IdBloque = b.Id and a.IdObra = o.Id and a.IdGrupo = g.Id and g.IdListaGrupo = lg.Id and ");
 			sb.Append("a.IdItem = i.Id and i.IdListaItem = li.Id and a.IdSubItem = si.Id ");
 			sb.AppendFormat("and pr.Id ={0} ", IdProyecto);
@@ -150,6 +150,44 @@ namespace BuildingMonitor.Data
 				}
 			}
 			
+			return ds;
+		}
+
+		public static DataSet DetalleAvance(int IdProyecto, int IdBloque, int IdObra, int IdGrupo, int IdContratista, int IdContrato)
+		{
+			StringBuilder sb = new StringBuilder();
+			
+			sb.Append("SELECT  ");
+			sb.Append("convert(nvarchar(5),a.IdProyecto) + ' - ' + pr.Nombre Proyecto ");
+			sb.Append(",convert(nvarchar(5),co.Id) + ' - ' + co.Nombre Contratista ");
+			sb.Append(",convert(nvarchar(5),a.IdBloque) + ' - ' + b.Nombre Bloque ");
+			sb.Append(",convert(nvarchar(5),a.IdObra) + ' - ' + o.Nombre Obra ");
+			sb.Append(",convert(nvarchar(5),a.IdGrupo) + ' - ' + lg.Nombre Grupo ");
+			sb.Append(",convert(nvarchar(5),a.IdItem) + ' - ' + li.Nombre Item ");
+			sb.Append(",convert(nvarchar(5),a.IdSubItem) + ' - ' + si.Nombre SubItem ");
+			sb.Append(",a.AvanceActual - a.AvanceInicial Avance ");
+			sb.Append(",convert(nvarchar(10),convert(decimal(15,2),si.Cantidad*(a.AvanceActual - a.AvanceInicial)/100)) + li.Unidad Cantidad ");
+			sb.Append(",(a.AvanceActual - a.AvanceInicial)*cd.Precio/100 SubTotal ");
+			
+			sb.Append("FROM uAvance a, uContrato c, uContratista co, uProyecto pr,uBloque b, uObra o, uGrupo g, uListaGrupo lg, uItem i, uListaItem li, uSubItem si, uContratoDetalle cd  ");
+			sb.Append("WHERE a.IdContrato = c.Id and c.IdContratista = co.Id and pr.Id = a.IdProyecto and b.Id = a.IdBloque and o.Id = a.IdObra and g.Id = a.IdGrupo and lg.Id = g.IdListaGrupo and i.Id = a.IdItem and i.IdListaItem = li.Id and si.Id = a.IdSubItem and cd.IdProyecto = a.IdProyecto and cd.IdBloque = a.IdBloque and cd.IdObra = a.IdObra and cd.IdGrupo = a.IdGrupo and cd.IdItem = a.IdItem and cd.IdSubItem = a.IdSubItem and cd.IdContrato = c.Id and c.IdContratista = co.Id ");
+			sb.AppendFormat("and pr.Id ={0} ", IdProyecto);
+			if (IdBloque > 0) sb.AppendFormat("and b.Id ={0} ", IdBloque);
+			if (IdObra > 0) sb.AppendFormat("and o.Id ={0} ", IdObra);
+			if (IdGrupo > 0) sb.AppendFormat("and g.IdListaGrupo ={0} ", IdGrupo);
+			if (IdContratista > 0) sb.AppendFormat("and co.Id ={0} ", IdContratista);
+			if (IdContrato > 0) sb.AppendFormat("and c.Id ={0} ", IdContrato);
+			
+			sb.Append("ORDER by convert(nvarchar(5),a.IdProyecto) + ' - ' + pr.Nombre,");
+			sb.Append("convert(nvarchar(5),a.IdBloque) + ' - ' + b.Nombre,");
+			sb.Append("convert(nvarchar(5),a.IdObra) + ' - ' + o.Nombre,");
+			sb.Append("convert(nvarchar(5),a.IdGrupo) + ' - ' + lg.Nombre,");
+			sb.Append("convert(nvarchar(5),a.IdItem) + ' - ' + li.Nombre,");
+			sb.Append("a.Id,");
+			sb.Append("convert(nvarchar(5),a.IdSubItem) + ' - ' + si.Nombre");
+			
+			SqlParameterHelper sph = new SqlParameterHelper(DBHelper.Instance.ConnectionString, sb.ToString(), CommandType.Text, 0);			
+			DataSet ds = sph.ExecuteDataset();
 			return ds;
 		}
 
